029a7f2210af8bec6f80b3045059dd69d3caae73
Fatima Muhammad <your-email@example.com>
2026-09-05 01:36:29 +0500

    connected to docker and created some tables

---
+++ src/db.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+# convo with the databae is done through this sessionmkaer
+sessionLocal = sessionmaker    (bind=engine, autoflush=False,autocommit=False)
+
+
+class Base(DeclarativeBase):
+    pass
--- src/models.py
+++ src/models.py
@@ -1,2 +1,18 @@
-from sqlalchemy import Column, Integer, String, DateTime, FLoat, ForeignKey, Enum
+from sqlalchemy import Column, Integer, String, DateTime, Float, ForeignKey, Enum
+from sqlalchemy import DateTime, String
+from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime
 
+from .db import Base
+
+class Tenant(Base):
+    __tablename__ = "tenants"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(255), nullable=False)
+
+    created_at: Mapped[datetime] = mapped_column(DateTime,default=datetime.utcnow,nullable=False)
+
+
+# class Plan(Base):
+#     __tablename__ = "plan"
